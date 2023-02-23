1da842b526769af0d92687718814dae068890b51
UZABUMWANA Fiston <youfistalline11@gmail.com>
2023-02-22 19:53:03 -0800

    allll

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,57 +1,25 @@
 #include <stdio.h>
-
 /**
- * main - Computes and prints the sum of all the multiples of 3 or 5 below
- *        1024 (excluded), followed by a new line.
- *
- * Return: Always 0.
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
  */
 int main(void)
 {
-    int count;
-    unsigned long fibl, fib2 = 1, sum;
-    unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-    unsigned long half1, half2;
+	int i = 0;
+	unsigned long int a = 0, b = 1, next = 0;
 
-    for (count = 0; count < 92; count++)
-    {
-        sum = fibl + fib2;
-        printf("%lu", sum);
-        fibl = fib2;
-        fib2 = sum;
-        if (count != 91)
-            printf(", ");
-    }
-
-    fibl_half1 = fib1 / 100000;
-    fib2_half1 = fib2 / 10000000000;
-    fibl_half2 = fib1 % 10000000000;
-    fib2_half2 = fib2 % 10000000000;
-
-    for (count = 93; count < 99; count++)
-    {
-        half1 = fib1_half1 + fib2_half1;
-        half2 = fib1_half2 + fib2_half2;
-
-        if (fib1_half2 + fib2_half2 > 9999999999)
-        {
-            half1 += 1;
-            half2 = half2 % 10000000000;
-        }
-
-        printf("%lu%010lu", half1, half2);
-
-        if (count != 98)
-            printf(", ");
-
-        fibl_half1 = fib2_half1;
-        fib1_half2 = fib2_half2;
-        fib2_half1 = half1;
-        fib2_half2 = half2;
-    }
-
-    printf("\n");
-
-    return (0);
+	while (i < 98)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+		if (i < 97)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
-
