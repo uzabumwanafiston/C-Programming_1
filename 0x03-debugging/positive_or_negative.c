03fc07d7f4351f0f4f83adc151c97ff8cdb2c080
UZABUMWANA Fiston <youfistalline11@gmail.com>
2023-02-23 16:37:52 -0800

    all

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,12 +8,12 @@
  *
  * Return: void
  */
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
 	else
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 }
