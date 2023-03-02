b61faec8df00e4be47fb8609966ebf47cfba5cd1
UZABUMWANA Fiston <youfistalline11@gmail.com>
2023-03-02 15:41:08 -0800

    modified

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,48 +1,41 @@
 #include "main.h"
-#include <unistd.h>
-
+#include <stddef.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 /**
  * rot13 - Encodes a string using rot13.
- * @s: The string to be encoded.
+ * @str: The string to be encoded.
  *
  * Return: A pointer to the encoded string.
  */
-char *rot13(char *s)
+char *rot13(char *str)
 {
-	char *p = s;
-	int i, j;
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-
-	for (i = 0; s[i] != '\0'; i++)
+	int i;
+	char *result = str;
+	/* Loop through each character in the string */
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; alphabet[j] != '\0'; j++)
+		/* If the character is a letter */
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
 		{
-			if (s[i] == alphabet[j])
-			{
-				_putchar(rot13[j]);
-				break;
-			}
-			else if (s[i] == rot13[j])
-			{
-				_putchar(alphabet[j]);
-				break;
-			}
+			result[i] = str[i] + 13;
 		}
-		if (alphabet[j] == '\0' && rot13[j] == '\0')
+		else if ((str[i] >= 'n' && str[i] <= 'z') ||
+				(str[i] >= 'N' && str[i] <= 'Z'))
 		{
-			_putchar(s[i]);
+			result[i] = str[i] - 13;
+		}
+		else
+		{
+			/* Not a letter, do not modify */
+			result[i] = str[i];
 		}
 	}
-	return (p);
+	return (result);
 }
