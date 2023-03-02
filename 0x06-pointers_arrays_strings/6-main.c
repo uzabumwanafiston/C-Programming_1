1883a36be0c191d6567bb28b6d13dacc3222e335
UZABUMWANA Fiston <youfistalline11@gmail.com>
2023-03-02 13:46:12 -0800

    added

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  *
@@ -8,11 +7,11 @@
  */
 int main(void)
 {
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on whatcomes.\n hello world! hello-world 0123456hello world\thello world.hello world\n";
+	char *ptr;
 
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
 }
