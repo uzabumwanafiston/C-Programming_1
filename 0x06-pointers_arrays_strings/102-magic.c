c1eaaf074d120ee0c49a9efad7242111fbff4dbb
UZABUMWANA Fiston <youfistalline11@gmail.com>
2023-03-02 15:58:37 -0800

    102-magic was added

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98; /* Add one line to set a[2] to 98 */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
