5840a7809fc76be400fd31b382c5d2dd63392025
UZABUMWANA Fiston <youfistalline11@gmail.com>
2023-03-02 14:49:02 -0800

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @s: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *s)
+{
+	char *p = s;
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (s[i] == alphabet[j])
+			{
+				_putchar(rot13[j]);
+				break;
+			}
+			else if (s[i] == rot13[j])
+			{
+				_putchar(alphabet[j]);
+				break;
+			}
+		}
+		if (alphabet[j] == '\0' && rot13[j] == '\0')
+		{
+			_putchar(s[i]);
+		}
+	}
+	return (p);
+}
