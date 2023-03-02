81db058cb71ad12524a2b86aa069884a6493f39c
UZABUMWANA Fiston <youfistalline11@gmail.com>
2023-03-02 09:52:29 -0800

    0-main.c  0-strcat  0-strcat.c  main.h  _putchar.c created

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string to append to
+ * @src: source string to append
+ *
+ * Return: pointer to resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr;
+	*ptr = dest;
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (ptr);
+}
