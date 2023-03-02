7f67985368412816f7c9b9646bc06d0558cee8f5
UZABUMWANA Fiston <youfistalline11@gmail.com>
2023-03-02 13:25:57 -0800

    5-string_toupper.c created

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * @str: String to modify.
+ *
+ * Return: Pointer to the modified string.
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+	return (str);
+}
