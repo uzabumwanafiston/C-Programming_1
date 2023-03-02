c630f45fc7dfd466849689450c464bce13e87603
UZABUMWANA Fiston <youfistalline11@gmail.com>
2023-03-02 15:13:42 -0800

    added

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,22 @@
 #include "main.h"
 #include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 /**
  * rot13 - Encodes a string using rot13.
  * @s: The string to be encoded.
  *
  * Return: A pointer to the encoded string.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 char *rot13(char *s)
 {
 	char *p = s;
